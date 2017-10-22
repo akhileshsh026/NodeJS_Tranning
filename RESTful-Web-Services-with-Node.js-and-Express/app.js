03e1aee2c5906ab1b173aed6af5a58d158616c4b
DIM-X-GOLDENSER\akhil <akhileshsh026@hotmail.com>
2017-10-23 00:24:10 +0530

    done with rutes

--- RESTful-Web-Services-with-Node.js-and-Express/app.js
+++ RESTful-Web-Services-with-Node.js-and-Express/app.js
@@ -4,6 +4,16 @@ var app = express();
 
 var port = process.env.PORT || 3000;
 
+var bookrouter = express.Router();
+
+bookrouter.route('/Books')
+  .get(function(req,res){
+      var responsejson = {hello :"This is book APi"};
+
+      res.json(responsejson);
+  });
+app.use('/api',bookrouter);
+
 app.get('/',function(req,res){
         res.send('Welcome to my APi');
 });
