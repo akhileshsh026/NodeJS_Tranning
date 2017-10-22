e2e7e038994f9f906d2200605001d5653519a643
DIM-X-GOLDENSER\akhil <akhileshsh026@hotmail.com>
2017-10-22 16:38:39 +0530

    used the asynchronysly   file load method .

--- Web-App1/index.js
+++ Web-App1/index.js
@@ -4,21 +4,28 @@ var http = require('http');
 var express = require('express');
 const fs = require('fs');
 
-const configJSON = fs.readFileSync('./config.json');
-const config = JSON.parse(configJSON);
+// const configJSON = fs.readFileSync('./config.json');
 
-const app = express();
+fs.readFile('./config.json','utf8',function(err,data){
 
-app.use(express.static(config.webServer.folder));
+    const config = JSON.parse(data);
+    
+    const app = express();
+    
+    app.use(express.static(config.webServer.folder));
+    
+    const httpServer = http.createServer(app);
+    
+    httpServer.listen(config.webServer.port,function(err){
+        if(err)
+            {
+                console.log(err.message);
+                return;
+            }
+    
+        console.log(`server listing on ${config.webServer.port}`);
+    })
+});
 
-const httpServer = http.createServer(app);
+console.log('reading the files aysnchrnosysoluy');
 
-httpServer.listen(config.webServer.port,function(err){
-    if(err)
-        {
-            console.log(err.message);
-            return;
-        }
-
-    console.log(`server listing on ${config.webServer.port}`);
-})
