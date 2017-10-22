44c2b740e7cb007b7b4b78602a7c53dfb2337cb8
DIM-X-GOLDENSER\akhil <akhileshsh026@hotmail.com>
2017-10-22 16:22:30 +0530

    used the 1st web build sever with the help of express.

--- Web-App1/index.js
+++ Web-App1/index.js
@@ -1,1 +1,20 @@
-console.log(' Hello Akhilesh on NOde !');
+'use strict';
+
+var http = require('http');
+var express = require('express');
+
+const app = express();
+
+app.use(express.static('www'));
+
+const httpServer = http.createServer(app);
+
+httpServer.listen(3000,function(err){
+    if(err)
+        {
+            console.log(err.message);
+            return;
+        }
+
+    console.log('server listing on 3000');
+})
