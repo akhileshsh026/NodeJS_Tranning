b094d88186664b48c135445de94c23e8c4cb4010
Akhilesh Kumar Sahu <Akhileshsh028@outlook.com>
2017-10-27 11:13:29 +0530

    used the AkhileshKumar.sahu@outlook.com mongodb linux hosted web server . where the database is Dimentionx and accessing the employees collections.

--- MongoUnivercity/app.js
+++ MongoUnivercity/app.js
@@ -2,16 +2,16 @@ var MongoClient = require('mongodb').MongoClient,
    assert = require('assert');
 
   
-MongoClient.connect('mongodb://localhost:27017/video',function(err,db){
+MongoClient.connect('mongodb://52.187.19.241:27017/dimentionx',function(err,db){
 
  // The assert module provides a simple set of assertion tests that can be used to test invariants.
 assert.equal(null,err);
 console.log('SucessFully Connected to Server');
 
           // find docs in mongo
-   db.collection('movies').find({}).toArray(function(err,docs){
+   db.collection('employes').find({}).toArray(function(err,docs){
           docs.forEach(function(doc){
-            console.log(doc.title);
+            console.log(doc.name);
           });
 
           db.close();
