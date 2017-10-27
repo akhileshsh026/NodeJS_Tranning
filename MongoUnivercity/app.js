f424aeda75be13b0de427c2f580fd03b4103f31a
Akhilesh Kumar Sahu <Akhileshsh028@outlook.com>
2017-10-27 11:59:55 +0530

    Using the templete engine package ie consolidate where HTML engine inside is nunjucks , then nunjuckks is registed with express to display HTML files also we created the views directory where we have our view --dirname is default name provided by node for path .

--- MongoUnivercity/app.js
+++ MongoUnivercity/app.js
@@ -1,16 +1,22 @@
 var express = require('express'),
- app = express();
+    app = express(),
+    engines = require('consolidate');  // is for templete engine rest all things taken acre by this .
+
+    app.engine('html',engines.nunjucks);
+    app.set('view engine',' html');
+    app.set('Views',__dirname + '/Views');
 
 
- app.get('/',function(req,res){
-    res.send('Hello Friend all good');
- });
+    app.get('/',function (req,res){
+                 res.render('hello ' + {'name': 'templetes' })
+        })
 
- app.use(function(req,res){
-       res.sendStatus(404);
- });
+    app.get(function (req,res) {
+         res.sendStatus(404)
+    })
+
 
  var server = app.listen(8000,function(){
-         // var port = server.address.port;
-     console.log('server is listing at 8000');
+     var port = server.address.port;
+     console.log('server is listing at ' + port);
  })
