aec51bab41f41cd6323314dd5d156e56309437ca
DIM-X-GOLDENSER\akhil <akhileshsh026@hotmail.com>
2017-10-28 01:26:51 +0530

    Finally done keep take care of the parameters.

--- MongoUnivercity/app.js
+++ MongoUnivercity/app.js
@@ -1,22 +1,24 @@
 var express = require('express'),
-    app = express(),
-    engines = require('consolidate');  // is for templete engine rest all things taken acre by this .
+app = express(),
+engines = require('consolidate');
 
-    app.engine('html',engines.nunjucks);
-    app.set('view engine','html');
-    app.set('views',__dirname + '/views');
+app.engine('html', engines.nunjucks);
+app.set('view engine', 'html');
+app.set('views', __dirname + '/views');
 
+app.get('/', function(req, res) {
+res.render( 'index',{ name : "Templates" });
+});
 
-    app.get('/',function (req,res){
-                 res.render('hello ' + {'name': 'templetes' });
-        })
+app.get('/Akhilesh',function(req,res){
+   res.render('Home',{work:'Akhielsh'});   // keep take care of 1st parameter where you have to show the html page that is in view .
+});
 
-    app.get(function (req,res) {
-         res.sendStatus(404)
-    })
+app.use(function(req, res){
+res.sendStatus(404); 
+});
 
-
- var server = app.listen(8000,function(){
-     var port = server.address.port;
-     console.log('server is listing at ' + port);
- })
+var server = app.listen(4000, function() {
+var port = server.address().port;
+console.log('Express server listening on port %s', port);
+});
