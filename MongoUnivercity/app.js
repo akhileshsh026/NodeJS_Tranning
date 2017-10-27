a36328e8b13581ee3eb67bd4be0e7c5a1c1b91d8
akhileshsh026@hotmail.com <akhileshsh026@hotmail.com>
2017-10-27 09:01:23 +0530

    intro to node.js drivres

--- MongoUnivercity/app.js
+++ MongoUnivercity/app.js
@@ -1,3 +1,20 @@
-var express = require('express'),
-  cons = require('consolidate'),
-   mongodb = require('mongodb');
+var MongoClient = require('mongodb').MongoClient,
+   assert = require('assert');
+
+  
+MongoClient.connect('mongodb://localhost:27017/video',function(err,db){
+
+assert.equal(null,err);
+console.log('SucessFully Connected to Server');
+
+          // find docs in mongo
+   db.collection('movies').find({}).toArray(function(err,docs){
+          docs.forEach(function(doc){
+            console.log(doc.title);
+          });
+
+          db.close();
+   });
+
+      console.log('find() colled');
+})
