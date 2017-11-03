fa28c06d93f0b5744b92129296464a8a564a6c88
DIM-X-GOLDENSER\akhil <akhileshsh026@hotmail.com>
2017-11-04 01:58:19 +0530

    Another way of doing the same thing but with less code.

--- MongoUnivercity Season 3/Episode1 Find() and Cursors in MongoDB Drivers/app.js
+++ MongoUnivercity Season 3/Episode1 Find() and Cursors in MongoDB Drivers/app.js
@@ -7,16 +7,22 @@ MongoClient.connect('mongodb://52.187.19.241:27017/CrunchDB',function(err,db){
     console.log("Connected Sucessfully");
 
     var Query = {"category_code":"biotech"};
-
-    db.collection('Companies').find(Query).toArray(function(err,docs){
+    var Cursor = db.collection('Companies').find(Query);
          
         assert.equal(err,null);
-        assert.notEqual(docs.length,0);
+        assert.notEqual(Cursor.length,0);
 
-        docs.forEach(function(doc){
+        Cursor.forEach(
+            function(doc){
             console.log(doc.name+'is a'+doc.category_code+" Company.");
-        });
+                         },
+                  function(err)
+                  {
+                      assert.equal(err,null);
+                      return db.close();
+                  }
+    
+             );
             
-        db.close();
+        
     });
-});
