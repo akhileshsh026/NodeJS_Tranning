e7713a984c9620c8f6445557d62d402c869bc720
DIM-X-GOLDENSER\akhil <akhileshsh026@hotmail.com>
2017-11-06 00:30:23 +0530

    Quiz for projecctions

--- MongoUnivercity Season 3/Episode1 Find() and Cursors in MongoDB Drivers/app.js
+++ MongoUnivercity Season 3/Episode1 Find() and Cursors in MongoDB Drivers/app.js
@@ -1,29 +1,28 @@
 var MongoClient = require('mongodb').MongoClient,
-  assert = require('assert');   
+assert = require('assert');
 
 
-MongoClient.connect('mongodb://52.187.19.241:27017/CrunchDB',function(err,db){
-    assert.equal(err,null);
-    console.log("Connected Sucessfully");
+MongoClient.connect('mongodb://52.187.19.241:27017/CrunchDB', function(err, db) {
 
-    var Query = {"category_code":"biotech"};
-    var projection = {"name":1,"category_code":1,"_id":0};
-    
-    var Cursor = db.collection('Companies').find(Query);
-    Cursor.project(projection); 
+assert.equal(err, null);
+console.log("Successfully connected to MongoDB.");
 
-        Cursor.forEach(
-                function(doc)
-                {
-                    console.log(doc.name+' is a '+doc.category_code+" Company.");
-                },
-                  function(err)
-                  {
-                      assert.equal(err,null);
-                      return db.close();
-                  }
-    
-             );
-            
-        
-    });
+var query = {"founded_year": 2010};
+
+var cursor = db.collection('Companies').find(query);
+
+/* TODO: Write your line of code here. */
+var projections = {"number_of_employees":1,"name":1,"_id":0};
+cursor.project(projections);
+
+cursor.forEach(
+    function(doc) {
+        console.log(doc.name + " has " + doc.number_of_employees + " employees.");
+    },
+    function(err) {
+        assert.equal(err, null);
+        return db.close();
+    }
+);
+
+});
