bc7125e893cd7a43467650d80dca92fbf4d18ba8
DIM-X-GOLDENSER\akhil <akhileshsh026@hotmail.com>
2017-11-13 22:16:59 +0530

    parse() in commad  line not supporting

--- MongoUnivercity Season 3/Episode1 Find() and Cursors in MongoDB Drivers/app.js
+++ MongoUnivercity Season 3/Episode1 Find() and Cursors in MongoDB Drivers/app.js
@@ -1,27 +1,77 @@
 var MongoClient = require('mongodb').MongoClient,
+commandLineArgs = require('command-line-args'), 
 assert = require('assert');
 
 
-MongoClient.connect('mongodb://52.187.19.241:27017/CrunchDB', function(err, db) {
+var options = commandLineOptions();
+
+MongoClient.connect('mongodb://localhost:27017/crunchbase', function(err, db) {
 
 assert.equal(err, null);
 console.log("Successfully connected to MongoDB.");
 
-var query = {"founded_year": 2010};
+var query = queryDocument(options);
+var projection = {"_id": 1, "name": 1, "founded_year": 1,
+                  "number_of_employees": 1, "crunchbase_url": 1};
 
-var cursor = db.collection('Companies').find(query);
-
-var projections = {"number_of_employees":1,"name":1,"_id":0};  
-cursor.project(projections); // or cursor.project({"number_of_employees":1,"name":1,"_id":0}); 
+var cursor = db.collection('companies').find(query, projection);
+var numMatches = 0;
 
 cursor.forEach(
     function(doc) {
-        console.log(doc.name + " has " + doc.number_of_employees + " employees.");
+        numMatches = numMatches + 1;
+        console.log( doc );
     },
     function(err) {
         assert.equal(err, null);
+        console.log("Our query was:" + JSON.stringify(query));
+        console.log("Matching documents: " + numMatches);
         return db.close();
     }
 );
 
 });
+
+
+function queryDocument(options) {
+
+console.log(options);
+
+var query = {
+    "founded_year": {
+        "$gte": options.firstYear,
+        "$lte": options.lastYear
+    }
+};
+
+if ("employees" in options) {
+    query.number_of_employees = { "$gte": options.employees };
+}
+    
+return query;
+
+}
+
+
+function commandLineOptions() {
+
+var cli = commandLineArgs([
+    { name: "firstYear", alias: "f", type: Number },
+    { name: "lastYear", alias: "l", type: Number },
+    { name: "employees", alias: "e", type: Number }
+]);
+
+var options = cli.parse()
+if ( !(("firstYear" in options) && ("lastYear" in options))) {
+    console.log(cli.getUsage({
+        title: "Usage",
+        description: "The first two options below are required. The rest are optional."
+    }));
+    process.exit();
+}
+
+return options;
+
+}
+
+
