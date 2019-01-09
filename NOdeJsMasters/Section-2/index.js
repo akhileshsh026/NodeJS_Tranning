de4bf40f3673d48dc9e03fc643b5a6a606635f87
Akhilesh Kumar Sahu <akhileshsh026@hotmail.com>
2019-01-09 15:27:55 +0530

    get payloads , comes as a stream to server from client , this is how streams are heandeled in the node.js #nodejs_Streams #nodedocumentation

--- NOdeJsMasters/Section-2/index.js
+++ NOdeJsMasters/Section-2/index.js
@@ -1,5 +1,6 @@
 var http = require('http');
 var url = require('url');
+var StringDecoder = require('string_decoder').StringDecoder; 
 
 var server = http.createServer(function (req,res) {
     var parseurl = url.parse(req.url,true);
@@ -12,9 +13,20 @@ var server = http.createServer(function (req,res) {
     // get the headers as an object
     var headervar = req.headers;
 
-    res.end('Hello World \n');
-    // console.log('Request receved in path: ' + trimedPath + ', With Method :' + method + ', With these query strings :' , queryString);
-    console.log('Req receved with these headers : ' , headervar);
+    // getting payloads
+    var decoders = new StringDecoder('utf-8');
+    var buffer = '';
+    
+    req.on('data',function (data) {
+       buffer += decoders.write(data); 
+    });  // this is payload but what happens when its end , can be done by the follwoing event 
+    req.on('end',function (end) {
+        buffer += decoders.end(); 
+        // the response shuold be under this event & even though there is no payload in the request this will work .
+        res.end('Hello World \n');
+        // console.log('Request receved in path: ' + trimedPath + ', With Method :' + method + ', With these query strings :' , queryString);
+        console.log('Req receved with these payload : ' , buffer);
+    });
 });
 
 server.listen(3000,function () {
