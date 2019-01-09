c48251f102a7ed37c792a2a0290a12b060bc6e2e
Akhilesh Kumar Sahu <akhileshsh026@hotmail.com>
2019-01-09 13:49:35 +0530

    Parsing the http request with the method name

--- NOdeJsMasters/Section-2/index.js
+++ NOdeJsMasters/Section-2/index.js
@@ -5,9 +5,9 @@ var server = http.createServer(function (req,res) {
     var parseurl = url.parse(req.url,true);
     var path = parseurl.pathname;
     var trimedPath = path.replace(/^\/+|\/+$/g,'');
-
+    var method = req.method.toLowerCase();
     res.end('Hello World \n');
-    console.log('Request receved in path: ' + trimedPath);
+    console.log('Request receved in path: ' + trimedPath + ' With Method :' + method);
 });
 
 server.listen(3000,function () {
