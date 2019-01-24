b1a8b884e2b04214f48e32f90dd7b2ac63aeccf5
Akhilesh Kumar Sahu <akhileshsh026@hotmail.com>
2019-01-24 17:46:13 +0530

    Route Handler

--- NOdeJsMasters/Section-2/index.js
+++ NOdeJsMasters/Section-2/index.js
@@ -22,10 +22,27 @@ var server = http.createServer(function (req,res) {
     });  // this is payload but what happens when its end , can be done by the follwoing event 
     req.on('end',function (end) {
         buffer += decoders.end(); 
-        // the response shuold be under this event & even though there is no payload in the request this will work .
+        
+        var chooseHandler = typeof(router[trimedPath]) !== router[trimedPath] : handlers.notFound;
+        
+        // constracut data obj to send to the handler
+         var data {
+             'trimedPath' : trimedPath,
+             'queryString' : queryString,
+             'method' : method,
+             'headers' : headers,
+             'payload' : buffer
+         };
+
+         // route the request to the handler specified in the router
+         chooseHandler(data,function(statusCode,payload) {
+             
+         })
+
         res.end('Hello World \n');
+        
         // console.log('Request receved in path: ' + trimedPath + ', With Method :' + method + ', With these query strings :' , queryString);
-        console.log('Req receved with these payload : ' , buffer);
+        // console.log('Req receved with these payload : ' , buffer);
     });
 });
 
@@ -33,3 +50,20 @@ server.listen(3000,function () {
     console.log('Server is listenting at 3000');
     
 });
+
+//define handlers
+var handlers = {};
+
+// sample handler
+handlers.sample = function(data,callback){
+    // callback a http status code & Payload object
+    callback(406),{'name':'My name is sampple handler'};
+};
+
+handlers.notFound = function (data,callback) {
+    callback(404);
+};
+//define routes
+ var router = {
+  'sample' : handlers.sample
+ };
